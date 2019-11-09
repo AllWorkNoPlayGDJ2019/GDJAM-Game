ca37f3a036fb22984af0ad20c71192a0b81d9f91
raspyweather <raspyweather@gmail.com>
2019-11-08 21:47:08 -0500

    initialized simple compiler pipeline

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+    entry: "./src/main.ts", //relative to root of the application
+    devServer: {
+    },
+    output: {
+        filename: 'app.bundle.js',
+        //   path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: './dist/index.html' //relative to root of the application
+        })
+    ]
+
+}
