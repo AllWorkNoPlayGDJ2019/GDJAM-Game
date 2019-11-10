07e133117cb6a5ace63875899d5db37bb2d5309c
raspyweather <raspyweather@gmail.com>
2019-11-10 01:08:26 -0500

    major doll changes

--- src/scenes/doll.ts
+++ src/scenes/doll.ts
@@ -1,3 +1,24 @@
 export class doll {
-    
+    constructor(
+        public sprite: PIXI.Sprite,
+        public isBeltMovable: boolean,
+        public parent: PIXI.Container,
+        private readonly dollKilled: (me: doll) => void
+    ) { }
+    kill() {
+        this.parent.removeChild(this.sprite);
+        this.dollKilled(this);
+    }
+    move(position: PIXI.Point) {
+        if (this.isBeltMovable===true) {
+            this.sprite.position.set(position.x, position.y);
+        }
+    }
+    interruptMovement() {
+        this.isBeltMovable = false;
+        console.log('interrupted');
+    }
+    getPosition() {
+        return this.sprite.position;
+    }
 }
